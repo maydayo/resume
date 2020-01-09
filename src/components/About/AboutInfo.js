0f73f5f26e2222829d6cd1dd659dd7c1a9309b20
maydayo <wanwisa.silanan@gmail.com>
2020-01-09 15:45:20 +0700

    change icon

--- src/components/About/AboutInfo.js
+++ src/components/About/AboutInfo.js
@@ -4,6 +4,7 @@ const AboutInfo = () => (
   <div className="about-info">
     I'm a self-motivated programmer.
     I found that coding is fun. I enjoy being challenged by projects which make me keep learning new things and I love when my works really help someone's problems.
+    I’m also interested in working on a project where I get to collaborate with other developers, and share what I know too.
     <br/>
     Here's a few technologies I've been working with recently.
   </div>
