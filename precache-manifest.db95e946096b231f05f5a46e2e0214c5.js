3a6105cb9641a1b98dcb29844519db5f94ba0920
maydayo <wanwisa.silanan@gmail.com>
2020-01-09 16:13:07 +0700

    Updates

---
+++ precache-manifest.db95e946096b231f05f5a46e2e0214c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9b6057d3bf615554821c1e79c02bd57",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "e905a3122b928fdffdb2",
+    "url": "/resume/static/css/2.7abef457.chunk.css"
+  },
+  {
+    "revision": "3ca19b9bfaebc4631581",
+    "url": "/resume/static/css/main.f1896ac2.chunk.css"
+  },
+  {
+    "revision": "e905a3122b928fdffdb2",
+    "url": "/resume/static/js/2.0a22a07e.chunk.js"
+  },
+  {
+    "revision": "3ca19b9bfaebc4631581",
+    "url": "/resume/static/js/main.151b7f22.chunk.js"
+  },
+  {
+    "revision": "699038ae0f72f46193a3",
+    "url": "/resume/static/js/runtime-main.dfb416d0.js"
+  },
+  {
+    "revision": "1392d90f753f465ef712642e30868f41",
+    "url": "/resume/static/media/maydayo_icon.1392d90f.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.efd1743e85065194d78533a9d1317564.js"
+  "/resume/precache-manifest.db95e946096b231f05f5a46e2e0214c5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/resume/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
