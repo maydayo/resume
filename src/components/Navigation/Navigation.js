253f7bbde0d56dc4827dd0b6f1ccc73c95f01c28
maydayo <wanwisa.silanan@gmail.com>
2020-01-09 07:06:46 +0700

    close nav when click

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,45 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import { navLinks } from "../../config";
 
-const Navigation = () => (
-  <div className="navigation">
-    <input type="checkbox" class="navigation__checkbox" id="navi-toggle" />
+const Navigation = () => {
+  const [checked, setChecked] = useState(true);
+  const handleOnChange = () => {
+    setChecked(!checked);
+  };
+  return (
+    <div className="navigation">
+      <input
+        type="checkbox"
+        class="navigation__checkbox"
+        checked={checked}
+        onChange={handleOnChange}
+        id="navi-toggle"
+      />
 
-    <label for="navi-toggle" class="navigation__button">
-      <span class="navigation__icon">&nbsp;</span>
-    </label>
+      <label for="navi-toggle" class="navigation__button">
+        <span class="navigation__icon">&nbsp;</span>
+      </label>
 
-    <div class="navigation__background">&nbsp;</div>
+      <div class="navigation__background">&nbsp;</div>
 
-    <nav class="navigation__nav">
-      <ul class="navigation__list">
-        <li class="navigation__item">
-          <a href="#" class="navigation__link">
-            <span>01</span>About Natous
-          </a>
-        </li>
-        <li class="navigation__item">
-          <a href="#" class="navigation__link">
-            <span>02</span>Your benfits
-          </a>
-        </li>
-        <li class="navigation__item">
-          <a href="#" class="navigation__link">
-            <span>03</span>Popular tours
-          </a>
-        </li>
-        <li class="navigation__item">
-          <a href="#" class="navigation__link">
-            <span>04</span>Stories
-          </a>
-        </li>
-        <li class="navigation__item">
-          <a href="#" class="navigation__link">
-            <span>05</span>Book now
-          </a>
-        </li>
-      </ul>
-    </nav>
-  </div>
-);
+      <nav class="navigation__nav">
+        <ul class="navigation__list">
+          {navLinks.map(nav => {
+            const { name, url } = nav;
+            return (
+              <li className="navigation__item" key={name}>
+                <a
+                  href={url}
+                  onClick={() => {
+                    setChecked(false);
+                  }}
+                  className="navigation__link"
+                >
+                  {name}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    </div>
+  );
+};
 
 export default Navigation;
